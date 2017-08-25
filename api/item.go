0c43eeb54baecc9c18a0a5b9ac8dcd12c1e13f98
Kevin Haines <kevin.haines@appsbroker.com>
2017-08-25 16:16:57 +0100

    item api started

--- api/item.go
+++ api/item.go
@@ -3,16 +3,12 @@ package api
 import (
 	"fmt"
 	"psychic-rat/ctr"
-	"psychic-rat/mdl/company"
 	"net/http"
+	"psychic-rat/mdl/company"
 )
 
 func itemHandler(writer http.ResponseWriter, request *http.Request) {
-	if request.Method == http.MethodGet {
-		handleItemGet(writer, request)
-		return
-	}
-	if request.Method != http.MethodPost {
+	if request.Method != http.MethodGet {
 		unsupportedMethod(writer)
 		return
 	}
@@ -21,22 +17,41 @@ func itemHandler(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	const (
-		Make    = "make"
-		Model   = "model"
-		Company = "company"
-	)
-	params, ok := extractFormParams(request.Form, Make, Model, Company)
-	if ! ok {
-		fmt.Fprintf(writer, "form parameters missing: got %v", params)
+	companyId := request.Form.Get("company")
+	if companyId == "" {
+		errorResponse(writer, fmt.Errorf("no company specified"))
+		return
 	}
-
-	err := ctr.GetController().Item().AddItem(params[Make], params[Model], company.Id(params[Company]))
-	if err != nil {
-		errorResponse(writer, err)
-	}
-
+	//items := getItemsForCompany(companyId)
 }
+
+type itemElements struct {
+	Id string `json:"id"`
+	Make string `json:"make"`
+	Model string `json:"model"`
+}
+
+func getItemsForCompany(companyId company.Id) (items []itemElements) {
+	return items
+}
+
+//func createItem() {
+//	const (
+//		Make    = "make"
+//		Model   = "model"
+//		Company = "company"
+//	)
+//	params, ok := extractFormParams(request.Form, Make, Model, Company)
+//	if ! ok {
+//		fmt.Fprintf(writer, "form parameters missing: got %v", params)
+//	}
+//
+//	err := ctr.GetController().Item().AddItem(params[Make], params[Model], company.Id(params[Company]))
+//	if err != nil {
+//		errorResponse(writer, err)
+//	}
+//
+//}
 
 func handleItemGet(writer http.ResponseWriter, request *http.Request) {
 	items := ctr.GetController().Item().ListItems()
