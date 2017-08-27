fee50fe74886cdab15eb0e3bec8b5262e85604dd
Kevin Haines <kevin.haines@appsbroker.com>
2017-08-27 15:22:43 +0100

    Bug in jq v1.3 vs 1.5

--- tests/create-pledge.sh
+++ tests/create-pledge.sh
@@ -11,6 +11,6 @@ itemId=`$CURL $ITEM_API?company=1 | jq -r .items[0].id`
 
 pledgePost=`printf '{"itemId":"%s"}' $itemId`
 
-set `$CURL -XPOST -d $pledgePost $PLEDGE_API | jq -r .pledges[length-1].id,.pledges[length-1].item.id`
+set `$CURL -XPOST -d $pledgePost $PLEDGE_API | jq -r .pledges[-1].id,.pledges[-1].item.id`
 
 [ "$itemId" = "$2" ] || (echo "expected item $itemId but got item $2 in pledge $1";exit 1)
