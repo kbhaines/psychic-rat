6d587f6787185fad87fac077aaa0a348c7272293
Kevin Haines <kevin.haines@appsbroker.com>
2017-10-05 16:18:19 +0100

    Added method handler

--- cmd/server/pages.go
+++ cmd/server/pages.go
@@ -15,6 +15,10 @@ type variables struct {
 
 type renderFunction func(writer http.ResponseWriter, templateName string, variables interface{})
 
+type handlerFunc func(http.ResponseWriter, *http.Request)
+
+type methodSelector map[string]handlerFunc
+
 var renderPage renderFunction = renderPageUsingTemplate
 
 func HomePageHandler(writer http.ResponseWriter, request *http.Request) {
@@ -33,46 +37,63 @@ func SignInPageHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func PledgePageHandler(writer http.ResponseWriter, request *http.Request) {
+	selector := map[string]handlerFunc{
+		"GET":  pledgeGetHandler,
+		"POST": pledgePostHandler,
+	}
+	execHandlerForMethod(selector, writer, request)
+}
+
+func pledgeGetHandler(writer http.ResponseWriter, request *http.Request) {
 	report, err := apis.Item.ListItems()
 	if err != nil {
 		log.Fatal(err)
 	}
+	vars := variables{Username: "Kevin", Items: report.Items}
+	renderPage(writer, "pledge.html.tmpl", vars)
+}
 
-	if request.Method == "GET" {
-		vars := variables{Username: "Kevin", Items: report.Items}
-		renderPage(writer, "pledge.html.tmpl", vars)
+func pledgePostHandler(writer http.ResponseWriter, request *http.Request) {
+	if err := request.ParseForm(); err != nil {
+		log.Print(err)
 		return
 	}
-
-	if request.Method == "POST" {
-		if err := request.ParseForm(); err != nil {
-			log.Print(err)
-			return
-		}
-		log.Printf("request = %+v\n", request)
-		log.Printf("request.Form= %+v\n", request.Form)
-		itemId := mdl.Id(request.FormValue("item"))
-		if itemId == "" {
-			log.Print("item field not passed")
-			return
-		}
-		if _, err := apis.Item.GetById(itemId); err != nil {
-			log.Printf("error looking up item %v : %v", itemId, err)
-			return
-		}
-		log.Printf("pledge item %v from user", itemId)
-		userId := mdl.Id("1234")
-		plId, err := apis.Pledge.NewPledge(itemId, userId)
-		if err != nil {
-			log.Print("unable to pledge : ", err)
-			return
-		}
-		log.Printf("pledge %v created", plId)
+	log.Printf("request = %+v\n", request)
+	log.Printf("request.Form= %+v\n", request.Form)
+	itemId := mdl.Id(request.FormValue("item"))
+	if itemId == "" {
+		log.Print("item field not passed")
 		return
 	}
+	if _, err := apis.Item.GetById(itemId); err != nil {
+		log.Printf("error looking up item %v : %v", itemId, err)
+		return
+	}
+	log.Printf("pledge item %v from user", itemId)
+	userId := getUserIdFromRequest(request)
+	plId, err := apis.Pledge.NewPledge(itemId, userId)
+	if err != nil {
+		log.Print("unable to pledge : ", err)
+		return
+	}
+	log.Printf("pledge %v created", plId)
+	return
 }
 
 func ThanksPageHandler(writer http.ResponseWriter, request *http.Request) {
 	vars := variables{Username: "Kevin"}
 	renderPage(writer, "thanks.html.tmpl", vars)
+}
+
+func getUserIdFromRequest(request *http.Request) mdl.Id {
+	return mdl.Id("1234")
+}
+
+func execHandlerForMethod(selector methodSelector, writer http.ResponseWriter, request *http.Request) {
+	f, exists := selector[request.Method]
+	if !exists {
+		log.Print("invalid method in request ", request)
+		return
+	}
+	f(writer, request)
 }
