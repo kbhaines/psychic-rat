a9b2166b64415483c7e975ca6f6163ebd9a87004
Kevin Haines <kevin.haines@appsbroker.com>
2017-11-11 13:03:20 +0000

    Added company sql stuff

--- sqldb/main.go
+++ sqldb/main.go
@@ -2,16 +2,64 @@ package sqldb
 
 import (
 	"database/sql"
+	"os"
+	"psychic-rat/mdl"
+	"psychic-rat/types"
+	"strconv"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type DB *sql.DB
+type DB struct {
+	*sql.DB
+}
 
-func NewDB(name string) (DB, error) {
+func NewDB(name string) (*DB, error) {
+	os.Remove(name)
 	db, err := sql.Open("sqlite3", name)
 	if err != nil {
 		return nil, err
 	}
-	return DB(db), nil
+	createSchema(db)
+	return &DB{db}, nil
+}
+
+func createSchema(db *sql.DB) error {
+	stmt := `
+	create table companies (id integer primary key, name string);
+	`
+	_, err := db.Exec(stmt)
+	return err
+}
+
+func (d *DB) NewCompany(c mdl.Company) error {
+	stmt, err := d.Prepare("insert into companies(name) values(?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(c.Name)
+	return err
+}
+
+func (d *DB) GetCompanies() (types.CompanyListing, error) {
+	rows, err := d.Query("select id, name from companies")
+	result := types.CompanyListing{}
+	if err != nil {
+		return result, err
+	}
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			return result, err
+		}
+		result.Companies = append(result.Companies, types.CompanyElement{Id: mdl.ID(strconv.Itoa(id)), Name: name})
+	}
+	return result, nil
+}
+
+func (d *DB) GetById(mdl.ID) (types.CompanyElement, error) {
+	panic("not implemented")
 }
