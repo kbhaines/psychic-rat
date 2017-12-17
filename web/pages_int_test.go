8e6980745b841689076aa48d74ad89d2a12ab1ae
Kevin Haines <kevin.haines@appsbroker.com>
2017-12-17 11:57:44 +0000

    tidy test

--- web/pages_int_test.go
+++ web/pages_int_test.go
@@ -420,15 +420,9 @@ func TestLimitUserNewItems(t *testing.T) {
 func TestBadNewItemsPostInvalidAddParams(t *testing.T) {
 	server := newServer(t)
 	defer server.Close()
-	cookie := loginUser("test1", t)
+	cookie := loginUser("admin", t)
 	client := http.Client{Jar: cookie}
-	loadNewItems(client, t)
-
-	cookie = loginUser("admin", t)
-	client = http.Client{Jar: cookie}
-
 	v := url.Values{"add[]": []string{"abc"}}
 	resp, err := client.PostForm(testUrl+"/admin/newitems", v)
 	testPageStatus(resp, err, http.StatusBadRequest, t)
-
 }
