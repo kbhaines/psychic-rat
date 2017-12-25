063c9c8e1e15a9bda4eec93bf3e86eab192f6942
Kevin Haines <kevin.haines@appsbroker.com>
2017-12-25 10:31:51 +0000

    Sorted out pacakge names and added common functions file

---
+++ tests/common_test.go
@@ -0,0 +1,83 @@
+package tests
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/cookiejar"
+	"net/http/httptest"
+	"net/url"
+	"psychic-rat/sqldb"
+	"psychic-rat/web"
+	"psychic-rat/web/admin"
+	"psychic-rat/web/tmpl"
+	"strings"
+	"testing"
+)
+
+var testUrl string
+
+func newServer(t *testing.T) (*httptest.Server, *sqldb.DB) {
+	server := httptest.NewServer(web.Handler())
+	testUrl = server.URL
+	db := initDB(t)
+	apis := web.APIS{
+		Company: db,
+		Item:    db,
+		NewItem: db,
+		Pledge:  db,
+		User:    db,
+	}
+	web.Init(apis)
+	admin.Init(db, db, db, db)
+	tmpl.Init("../res/")
+	return server, db
+}
+
+func testPageStatus(resp *http.Response, err error, expectedCode int, t *testing.T) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("unexpected error", err)
+	}
+	if resp.StatusCode != expectedCode {
+		t.Fatalf("wanted %v, got %v. Response was %v", expectedCode, resp.StatusCode, resp)
+	}
+}
+
+func cleanUp(server *httptest.Server, db *sqldb.DB) {
+	server.Close()
+	closeDB(db)
+}
+
+func loginUser(user string, t *testing.T) http.CookieJar {
+	t.Helper()
+	resp, err := http.Get(testUrl + "/signin?u=" + user)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		t.Fatalf("unable to signin, error was %v, response %v", err, resp)
+	}
+	jar, _ := cookiejar.New(nil)
+	url, _ := url.Parse(testUrl)
+	jar.SetCookies(url, resp.Cookies())
+	return jar
+}
+
+func readResponseBody(resp *http.Response, t *testing.T) string {
+	t.Helper()
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return string(body)
+}
+
+func testStrings(body string, expectedStrings []string, t *testing.T) {
+	t.Helper()
+	for _, s := range expectedStrings {
+		if !strings.Contains(body, s) {
+			t.Errorf("body did not contain '%s'", s)
+		}
+	}
+	if t.Failed() {
+		t.Errorf("body was %s", body)
+	}
+}
