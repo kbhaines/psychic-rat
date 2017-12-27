26bcc1eb72c5b3c8f76f288cfba396cf315cb165
Kevin Haines <kevin.haines@appsbroker.com>
2017-12-27 16:28:55 +0000

    Added main.go

---
+++ auth0/main.go
@@ -0,0 +1,53 @@
+package auth0
+
+import (
+	"net/http"
+	"psychic-rat/sess"
+	"psychic-rat/types"
+)
+
+type (
+	Auth0Handler struct {
+		renderer    Renderer
+		domain      string
+		callbackURL string
+		clientID    string
+	}
+
+	Renderer interface {
+		Render(http.ResponseWriter, string, interface{}) error
+	}
+)
+
+func NewAuth0Handler(rendr Renderer, dom string, callback string, client string) *Auth0Handler {
+	return &Auth0Handler{
+		renderer:    rendr,
+		domain:      dom,
+		callbackURL: callback,
+		clientID:    client,
+	}
+}
+
+func (a *Auth0Handler) Handler(writer http.ResponseWriter, request *http.Request) {
+	vars := struct {
+		Auth0Domain      string
+		Auth0CallbackURL string
+		Auth0ClientId    string
+		User             types.User
+	}{
+		Auth0Domain:      a.domain,
+		Auth0CallbackURL: a.callbackURL,
+		Auth0ClientId:    a.clientID,
+	}
+	a.renderer.Render(writer, "signin-auth0.html.tmpl", vars)
+}
+
+func (a *Auth0Handler) GetLoggedInUser(r *http.Request) (*types.User, error) {
+	// TODO: nil is a smell. StoreReader/Writer interfaces.
+	s := sess.NewSessionStore(r, nil)
+	user, err := s.Get()
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
