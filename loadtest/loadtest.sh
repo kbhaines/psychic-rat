b616116eb4573faaaad3d7ab74f9611fcd5a20f4
Kevin Haines <kevin.haines@ntlworld.com>
2018-03-17 15:55:34 +0000

    Loadtest updates

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -2,6 +2,7 @@
 TARGET=$1
 DURATION=${2:-5s}
 
+RATE=100
 USER=user041
 
 if [ "$TARGET" == "" ];then
@@ -10,10 +11,10 @@ if [ "$TARGET" == "" ];then
 fi
 
 # Generate cookie
-curl  http://$USER:@localhost:8080/callback\?p=basic -c cookie > /dev/null
-curl  http://$USER:@localhost:8080/pledge -b cookie -c cookie > /dev/null
+curl http://$USER:@localhost:8080/callback\?p=basic -c cookie > /dev/null
+curl http://$USER:@localhost:8080/pledge -b cookie -c cookie > /dev/null
 
 COOKIE=`awk '/localhost/{ print $7}' cookie`
 
 sed "s/:target:/$TARGET/;s/:cookie:/$COOKIE/" targets.tmpl > targets
-$HOME/go/bin/vegeta attack -rate 100 -targets targets -duration=$DURATION | tee results.bin | $HOME/go/bin/vegeta report
+$HOME/go/bin/vegeta attack -rate $RATE -targets targets -duration=$DURATION | tee results.bin | $HOME/go/bin/vegeta report
