1e148f9e1f68a3abdd0fd0551c67f8e8a98c9f85
Kevin Haines <kevin.haines@ntlworld.com>
2018-03-17 15:34:51 +0000

    added cookie generation to load test

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -2,10 +2,18 @@
 TARGET=$1
 DURATION=${2:-5s}
 
+USER=user041
+
 if [ "$TARGET" == "" ];then
     echo "specify target"
     exit 1
 fi
 
-sed "s/:target:/$TARGET/" targets.tmpl > targets
+# Generate cookie
+curl  http://$USER:@localhost:8080/callback\?p=basic -c cookie > /dev/null
+curl  http://$USER:@localhost:8080/pledge -b cookie -c cookie > /dev/null
+
+COOKIE=`awk '/localhost/{ print $7}' cookie`
+
+sed "s/:target:/$TARGET/;s/:cookie:/$COOKIE/" targets.tmpl > targets
 $HOME/go/bin/vegeta attack -rate 100 -targets targets -duration=$DURATION | tee results.bin | $HOME/go/bin/vegeta report
