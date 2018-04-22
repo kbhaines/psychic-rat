8a96bae2447f622f08ff233d975c41f6539ec371
Kevin Haines <kevin.haines@ntlworld.com>
2018-04-22 11:44:44 +0100

    added ec2-provision.sh

---
+++ scripts/ec2-provision.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+if [ $# -ne 3 ];then
+    echo "Usage: $0 <keyfile> <aws-keyname> <instance-type>"
+    exit
+fi
+
+KEYFILE=$1
+AWSKEY=$2
+TYPE=$3
+IMAGE_ID=ami-f4f21593
+
+set -x
+set -e
+
+start_instance() {
+    iid=`aws ec2 run-instances --image-id $IMAGE_ID --security-groups webserver --instance-type $TYPE --key-name $AWSKEY --query "Instances[0].InstanceId"`
+    aws ec2 wait instance-running --instance-ids $iid
+    HOST=`aws ec2 describe-instances --instance-ids $iid --query Reservations[0].Instances[*].PublicDnsName`
+    SSH="ssh -i $KEYFILE ubuntu@$HOST"
+    while ! $SSH sudo curl https://get.docker.com \| sh;do
+        sleep 5
+    done
+    $SSH sudo adduser ubuntu docker \; sudo apt-get install sqlite3
+}
+
+start_instance
+#HOST=ec2-35-177-6-185.eu-west-2.compute.amazonaws.com
+
+SSH="ssh -i $KEYFILE ubuntu@$HOST"
+
+docker save pr:latest | gzip | $SSH gunzip \| docker load
+
+cat <<-'EOF' > container_init
+#!/bin/sh
+
+if [ $# -ne 1 ];then
+    echo Usage: $0 <domain>
+    exit 0
+fi
+
+DOMAIN=$1
+
+rm -rf ./res
+
+docker create --name res pr 
+docker cp res:/res ./res 
+docker rm res
+
+container_id=`docker run -d --name pr --env-file ./pr.env -v /home/ubuntu/cert:/cert -v /home/ubuntu/res:/res -v /home/ubuntu/pr.dat:/pr.dat -p 80:8080 -p 443:4443 pr /server.linux -listen :8080  -listenSSL :4443 -cert /cert/live/$DOMAIN/fullchain.pem -key /cert/live/$DOMAIN/privkey.pem`
+
+docker run --rm -v /home/ubuntu/cert:/etc/letsencrypt -v $PWD/res:/res certbot/certbot:latest certonly --staging --agree-tos --email kevin@rat.me.uk -n --webroot -w /res/ -d $DOMAIN
+
+docker restart pr
+EOF
+
+chmod +x container_init
+scp -i $KEYFILE pr.dat pr.env container_init ubuntu@$HOST:
+
+echo press enter when DNS is ready to serve from $HOST
+nslookup $HOST
+read
+
+$SSH ./container_init
